6bfb25740125f3f2cdf671a87235acf18acfd573
Vitor Waga Okada <vitorw.okada@gmail.com>
2026-02-05 00:07:42 -0500

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: 'export',
   distDir: 'dist',
+  basePath: '/VitorOkada.github.io',
+  assetPrefix: '/VitorOkada.github.io',
   images: {
     unoptimized: true,
   },
